c9a87053143b8203a4064f90b3c40b4a88792ada
Evgeen <38689271+Evgeen@users.noreply.github.com>
2018-04-24 16:57:52 +0300

    Update EvgenRoles.js

--- EvgenRoles.js
+++ EvgenRoles.js
@@ -1,14 +1,16 @@
 //Settings!
 const yourID = "197818493880631298"; //Instructions on how to get this: https://redd.it/40zgse
-const setupCMD = "!createroles"
+const setupCMD = "!roles"
 let initialMessage = `**React to the messages below to receive the associated role. If you would like to remove the role, simply remove your reaction!**`;
-const roles = ["English", "Russian"];
-const reactions = [":flag_gb:", ":flag_ru"];
+const roles = ["Russian", "English"];
+const reactions = [":flag_ru:", ":flag_gb:"];
+const botToken = ""; /*You'll have to set this yourself; read more
+                     here https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token*/
 
 //Load up the bot...
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-client.login(process.env.BOT_TOKEN);
+bot.login(botToken);
 
 //If there isn't a reaction for every role, scold the user!
 if (roles.length !== reactions.length) throw "Roles list and reactions list are not the same length!";
