c742edfd029180464ccd030536fb0f5b8fee45b9
Evgen <38689271+Evgeen@users.noreply.github.com>
2018-05-13 01:07:29 +0300

    Update Roles.js

--- Roles.js
+++ Roles.js
@@ -3,7 +3,7 @@ const yourID = "197818493880631298"; //Instructions on how to get this: https://
 const setupCMD = "=roles"
 let initialMessage = `**React to the messages below to receive the associated role. If you would like to remove the role, simply remove your reaction!**`;
 const roles = ["Updates", "Events", "Votes"];
-const reactions = ["\:clipboard", "\:clipboard", "\:clipboard"];
+const reactions = ["🇬🇧", "🇬🇧", "🇬🇧"];
 
 //Load up the bot...
 const Discord = require('discord.js');
