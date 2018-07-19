95c24de5ac0c4956f351fd3e97e7d324ac9f44db
Luke <41430563+Chuwaa@users.noreply.github.com>
2018-07-19 16:27:05 +0100

    Update Roles.js

--- Roles.js
+++ Roles.js
@@ -2,8 +2,8 @@
 const yourID = "369813876264534017"; //Instructions on how to get this: https://redd.it/40zgse
 const setupCMD = "=roles"
 let initialMessage = `**React to the messages below to receive the associated role. If you would like to remove the role, simply remove your reaction!**`;
-const roles = ["Updates", "Events", "Votes"];
-const reactions = ["📋", "🎈", "📊"];
+const roles = ["Updates", "Events", "Votes", "Spotify Listener", "Overwatch", "Pokemon", "Assassins Creed", "Final Fantasy", "Watchdogs", "Osu!", "Don't Starve Together", "Portal", "Minecraft"];
+const reactions = ["📋", "🎈", "📊", "💚", "🔫", "🐭", "🗡️", "🔪", "🗒️", "🎀", "🥩", "🧡", "🌿"];
 
 //Load up the bot...
 const Discord = require('discord.js');
