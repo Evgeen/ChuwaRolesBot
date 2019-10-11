b0828322a4d22f4cd424b3e246f63c451c2a3542
Luke <41430563+Chuwaa@users.noreply.github.com>
2019-10-11 20:18:21 +0100

    Update Roles.js

--- Roles.js
+++ Roles.js
@@ -1,5 +1,5 @@
 //Settings!
-const yourID = "369813876264534017"; //Instructions on how to get this: https://redd.it/40zgse
+const yourID = "369813876264534017"; //Instructions on how to get this: https://redd.it/40zgsee
 const setupCMD = "=roles"
 let initialMessage = `**React to the messages below to receive the associated role. If you would like to remove the role, simply remove your reaction!**`;
 const roles = ["Born_to_Die", "Lover", "Ari_Stan", "Queen"];
